15c057c63e2e78ec6021127a1d30705b6350d62a
Vitaly Kochetov <kochetovit@gmail.com>
2022-11-22 21:11:01 +0300

    remove newlines before return

--- parser/arabic.go
+++ parser/arabic.go
@@ -8,7 +8,6 @@ type ArabicParser struct{}
 
 func (p *ArabicParser) Parse(inputStr string, operation *string, num *float64) error {
 	if _, err := fmt.Sscanf(inputStr, "%s %f", operation, num); err != nil {
-
 		return err
 	}
 
--- parser/roman.go
+++ parser/roman.go
@@ -8,7 +8,6 @@ type RomanParser struct{}
 
 func (p *RomanParser) Parse(strToParse string, operation *string, num *float64) error {
 	if _, err := fmt.Sscanf(strToParse, "%s %s", operation, num); err != nil {
-		
 		return err
 	}
 
